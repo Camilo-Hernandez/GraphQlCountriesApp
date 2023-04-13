b7f0e72ff400561d1e5e05bd8eef8be050f21bd2
Camilo Hernández Ruiz <camihruiz24@gmail.com>
2023-04-13 14:12:12 -0500

    Añadido el esquema de graphqls

--- app/src/main/java/com/plcoding/graphqlcountriesapp/MainActivity.kt
+++ app/src/main/java/com/plcoding/graphqlcountriesapp/MainActivity.kt
@@ -11,7 +11,8 @@ import com.plcoding.graphqlcountriesapp.presentation.CountriesViewModel
 import com.plcoding.graphqlcountriesapp.ui.theme.GraphQlCountriesAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint // We want to inject dependencies from Hilt a los Componentes Android que los necesiten, como los activities, broadcast receivers o los servicios.
+// Queremos inyectar las dependencias de Hilt a los Componentes Android que los necesiten, como los activities, broadcast receivers o los servicios.
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
